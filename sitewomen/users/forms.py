4ee1a94875c4fb211b5243b5273a21135adb8ee6
yevhen_skotarenko <jeny797@gmail.com>
2025-03-19 17:09:48 +0200

    lesson 9.4

--- sitewomen/users/forms.py
+++ sitewomen/users/forms.py
@@ -1,7 +1,12 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 
 
-class LoginUserForm(forms.Form):
+class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label='Логин', max_length=100,
                                widget=forms.TextInput(attrs={'class': 'form-input'}))
     password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
+
+    # class Meta:
+    #     model = get_user_model()
+    #     fields = ('username', 'password')
--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -1,25 +1,9 @@
-from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
-from django.urls import reverse
+from django.contrib.auth.views import LoginView
 
 from .forms import LoginUserForm
 
 # Create your views here.
-
-def login_user(request):
-    if request.method == 'POST':
-        form = LoginUserForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('home'))
-        return HttpResponse('not logged in')
-    form = LoginUserForm()
-    return render(request, 'users/login.html', {"form": form})
-
-def logout_user(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('users:login'))
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'users/login.html'
+    extra_context = {'title': 'Авторизация'}
